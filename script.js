cf9d24fa8198a38c66daedb36bc56411f728999a
Saurabh Maurya <saurabhmaurya14031997@gmail.com>
2022-03-26 20:41:17 +0530

    adding differnt cities

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const monthName = ['January', 'Febuary','March','April','May', 'June','July','August','September','October','November','December'];
 const dayName = ['Sun','Mon','Tues','Wednes','Thurs','Fri','Satur'];
 
+
 // function for switching between 12hrs/24hrs format
-let value = true;
+var value = true;
 function toggleHrs(){
     if(value === true) {
         value = false;
@@ -14,20 +15,28 @@ function toggleHrs(){
 
 // update time function
 function updateTime(){
+
   const serverTime = new Date();
 
 // storing data from the Date Obj
-  const hours = serverTime.getHours();
-  const minutes = serverTime.getMinutes();
-  const seconds = serverTime.getSeconds();
-  const milliseconds = serverTime.getMilliseconds();
-  const day = serverTime.getDay();
-  const month = serverTime.getMonth();
-  const year = serverTime.getFullYear();
+  var hour = serverTime.getHours();
+  var min = serverTime.getMinutes();
+  var seconds = serverTime.getSeconds();
+  var milliseconds = serverTime.getMilliseconds();
+  var day = serverTime.getDay();
+  var month = serverTime.getMonth();
+  var year = serverTime.getFullYear();
+
+// updating values according to the city
+let updatedValues = cityTime();
+var hours = hour + updatedValues[0];
+var minutes = min + updatedValues[1];
 
 // manipulating the data retrieved from Date(Obj)-->
-  const centiseconds = parseInt(milliseconds/100);
-  let hourFor = 0;
+  var centiseconds = parseInt(milliseconds/100);
+
+
+  var hourFor = 0;
 
  // 12/24 hrs format and display of the meridian 
   if(value === true){
@@ -42,7 +51,6 @@ function updateTime(){
     document.getElementById('toggleHrs').innerText='12 Hr'; 
   }
 
-
   document.getElementById('hr').innerText = hourFor>9 ? hourFor:'0'+hourFor;
   document.getElementById('min').innerText = minutes>9 ? minutes:'0'+minutes ;
   document.getElementById('sec').innerText = seconds>9 ? seconds:'0'+seconds ;
@@ -56,8 +64,18 @@ function updateTime(){
 setInterval(updateTime,100);
 
 //function for viewing other time zones
-function changeTimeZone(){
-
+// india has +5:30 with GMT 
+// refernce is india
+function cityTime(){
+  const city = document.getElementById('city').value
+  switch(city){
+    
+    case 'india': return [0,0];
+    case 'london': return [-5,-30];
+    case 'beijing': return[3, 0];
+    case 'paris': return[-4,-30];
+    case 'new_york': return [-9,-30];        
+  }
 };
 
 
