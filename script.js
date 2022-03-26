e60edd668cca9ce206b716ef035eb3711953b907
Saurabh Maurya <saurabhmaurya14031997@gmail.com>
2022-03-26 15:12:45 +0530

    refresh rate 100ms

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const serverTime = new Date();
-console.log(serverTime);
-
-// storing data from the Date Obj
-const hours = serverTime.getHours();
-const minutes = serverTime.getMinutes();
-const seconds = serverTime.getSeconds();
-const milliseconds = serverTime.getMilliseconds();
-const day = serverTime.getDay();
-const month = serverTime.getMonth();
-const year = serverTime.getFullYear();
-
-// manipulating the data retrieved from Date(Obj)-->
+// day and month array-->
 const monthName = ['January', 'Febuary','March','April','May', 'June','July','August','September','October','November','December'];
 const dayName = ['Sun','Mon','Tues','Wednes','Thurs','Fri','Satur'];
-const centiseconds = parseInt(milliseconds/100);
-let hourFor = hours >12 ? hours-12:hours;;
 
 
 // update time function
 function updateTime(){
+  const serverTime = new Date();
+
+// storing data from the Date Obj
+  const hours = serverTime.getHours();
+  const minutes = serverTime.getMinutes();
+  const seconds = serverTime.getSeconds();
+  const milliseconds = serverTime.getMilliseconds();
+  const day = serverTime.getDay();
+  const month = serverTime.getMonth();
+  const year = serverTime.getFullYear();
+
+// manipulating the data retrieved from Date(Obj)-->
+  const centiseconds = parseInt(milliseconds/100);
+  let hourFor = hours >12 ? hours-12:hours;
+
   document.getElementById('hr').innerText = hourFor>9 ? hourFor:'0'+hourFor;
   document.getElementById('min').innerText = minutes>9 ? minutes:'0'+minutes ;
   document.getElementById('sec').innerText = seconds>9 ? seconds:'0'+seconds ;
@@ -26,11 +27,8 @@ function updateTime(){
   document.getElementById('year').innerText = year;
   document.getElementById('month').innerText = monthName[month]+',';
   document.getElementById('day').innerText = dayName[day]+'day,';
-  document.getElementById('meridian').innerText = hours>11 ? 'PM' : 'AM';
-}
-
-updateTime();
-
+  document.getElementById('meridian').innerText = hours>11 ? 'PM' : 'AM'; 
+};
 
 // function for switching between 12hrs/24hrs format
 let value = true;
@@ -46,7 +44,10 @@ function toggleHrs(){
         document.getElementById('hr').innerText = hourFor>9 ? hourFor:'0'+hourFor;
     }
     
-};
+}; 
+
+
+setInterval(updateTime,100);
 
 //function for viewing other time zones
 function changeTimeZone(){
